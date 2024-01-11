a3b4236f44870f7e473d833dc3177986abe25f1d
Leroy <33508834+changshengliu312@users.noreply.github.com>
2024-01-11 16:23:29 +0800

    Update lgo.go

--- lgo.go
+++ lgo.go
@@ -2,15 +2,29 @@ package lgo
 
 import (
 	"fmt"
+	"io"
+	"lgo/log"
 	"lgo/utils"
 	"net"
 	"net/http"
+	"time"
 )
+
+// DefaultLogWriter log writer
+var DefaultLogWriter io.Writer
 
 func Run(port uint32) {
 	//DefaultServeMux.InitTimeout(time.Second * 10)
+	logWriter, err := log.NewLogWriter("lgo", 1073741824, 10)
+	if err != nil {
+		panic(err)
+	}
+	//DefaultLogWriter = conf.Log.Writer
+	log.SetOutput(logWriter)
+	DefaultLogWriter = logWriter
+	DefaultLogWriter.Write([]byte(fmt.Sprintf("==>-----------------lgo start at %s-----------------\n==>\n", time.Now())))
 
-	listenIP := utils.ItoIP(utils.GetLocalIP())
+	listenIP := utils.GetLocalIP()
 	addr := fmt.Sprintf("%s:%d", listenIP, port)
 	listenAddr, err := net.ResolveTCPAddr("tcp4", addr)
 	if err != nil {
